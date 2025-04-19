6eef879c959af366b80b0d969e635a293c8f98b9
kamel <kamelsamy3@gmail.com>
2025-04-19 16:47:03 +0200

    add http configration

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
-
+  tasks: string[] = [];
+  addTask(task: string): void {
+    this.tasks.push(task);
+  }
+  removeTask(task: string): void {
+    this.tasks = this.tasks.filter((t) => t !== task);
+  }
 }
