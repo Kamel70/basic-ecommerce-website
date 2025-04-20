ed58bf6d2a13bf818c96a2146be5d98fdf958b14
kamel <kamelsamy3@gmail.com>
2025-04-20 06:07:25 +0200

    modify details button of each product card to go to product details

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { IProduct } from '../../interfaces/iproduct';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -8,5 +9,9 @@ import { IProduct } from '../../interfaces/iproduct';
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
+  router = inject(Router);
   product = input.required<IProduct>();
+  goToDetails(id: number) {
+    this.router.navigate(['/products', id]);
+  }
 }
