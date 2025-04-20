1c6189d52f6c1e929146cdd79fcf624c1d78e559
kamel <kamelsamy3@gmail.com>
2025-04-20 14:39:42 +0200

    fix bugs and show cart products

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { IProduct, IproductApi } from '../interfaces/iproduct';
+import { CartItem } from '../interfaces/cart-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor() {}
+  constructor() {
+    this.loadCart();
+  }
   http = inject(HttpClient);
   // apiUrl = 'https://fakestoreapi.com/products';
   cartProducts: IProduct[] = [];
   cartProductsSignal = signal(this.cartProducts);
+  // private cartProducts: CartItem[] = [];
+  // cartProductsSignal = signal<CartItem[]>([]);
   getProducts() {
     return this.http.get<IproductApi>(
       'https://dummyjson.com/products?limit=10&skip=20'
@@ -22,10 +27,19 @@ export class ProductService {
 
   addToCart(product: IProduct) {
     this.cartProducts.push(product);
-    this.cartProductsSignal.update(() => this.cartProducts);
+    this.cartProductsSignal.set(this.cartProducts);
+    localStorage.setItem('cart', JSON.stringify(this.cartProducts));
   }
   removeFromCart(product: IProduct) {
     this.cartProducts.filter((p) => p.id !== product.id);
     this.cartProductsSignal.update(() => this.cartProducts);
+    localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+  }
+  loadCart() {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      this.cartProducts = JSON.parse(savedCart);
+      this.cartProductsSignal.set(this.cartProducts);
+    }
   }
 }
